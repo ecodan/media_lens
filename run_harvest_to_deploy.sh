2b3af6e3178fbdd2fc1c1a50722db23d00ff281b
dan <ecodan@hotmail.com>
2025-04-14 09:18:06 -0700

    Updated run scripts to include daily summarizer and 0700 schedule

--- run_harvest_to_deploy.sh
+++ run_harvest_to_deploy.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-python src/media_lens/runner.py run -s harvest extract interpret_weekly format deploy -o /Users/dan/dev/code/projects/python/media_lens/working/out
+python src/media_lens/runner.py run -s harvest extract summarize_daily interpret_weekly format deploy -o /Users/dan/dev/code/projects/python/media_lens/working/out
--- run_media_lens_scheduled.sh
+++ run_media_lens_scheduled.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-python src/media_lens/scheduler.py --time 17:00 --script ./run_harvest_to_deploy.sh
+python src/media_lens/scheduler.py --time 07:00 --script ./run_harvest_to_deploy.sh
